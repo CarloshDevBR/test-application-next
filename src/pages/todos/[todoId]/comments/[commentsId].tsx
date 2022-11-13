a7568769c60f47abb92d8b842bc39f112466a4f6
carlos.sortudox1@gamil.com <carlos.sortudox1@gamil.com>
2022-11-13 17:12:44 -0300

    id adicionado

--- src/pages/todos/[todoId]/comments/[commentsId].tsx
+++ src/pages/todos/[todoId]/comments/[commentsId].tsx
@@ -1,11 +1,19 @@
 import Link from "next/link"
 
+import { useRouter } from 'next/router'
+
 function Comment() {
+    const router = useRouter()
+
+    const todoId = router.query.todoId
+    const commentId = router.query.commentsId
+
     return (
         <>
-            <Link href="/todos/1">
+            <Link href={`/todos/${todoId}`}>
                 <a>Voltar</a>
             </Link>
+            <h1>Exibindo o comentário número: {commentId}</h1>
         </>
     )
 }
