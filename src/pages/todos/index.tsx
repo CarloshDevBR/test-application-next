966cf19c26a2e2d2087c67e218e463e7bf58b9dc
carlos.sortudox1@gamil.com <carlos.sortudox1@gamil.com>
2022-11-12 22:38:40 -0300

    nome alterado

---
+++ src/pages/todos/index.tsx
@@ -0,0 +1,37 @@
+import styles from '../../styles/todos.module.css'
+
+interface Item {
+  todos: {
+    map(arg0: (e: any) => JSX.Element): import("react").ReactNode;
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+}
+
+export async function getStaticProps() {
+    const data = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const todos = await data.json()
+
+    return {
+      props: { todos }
+    }
+}
+
+function Todo({ todos }: Item) {
+  return (
+    <>
+      <h1>Tarefas</h1>
+      <ul className={styles.todolist}>
+        {
+          todos.map((e) => (
+            <li key={e.id}>{e.id} - {e.title}</li>
+          ))
+        }
+      </ul>
+    </>
+  )
+}
+
+export default Todo
